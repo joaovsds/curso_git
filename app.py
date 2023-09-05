870e2d49da7958c4ec47a0a2ac517cce0b303b92
joao vds <joao.ggww@gmail.com>
2023-09-05 17:32:40 -0300

    criado arquivo de extracao e ajustando arquivo principal

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
+from src.extraction import load_data
+
+st.st_page_config(layout='wide')
+
+def main():
+    df = load_data
+
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    main()
